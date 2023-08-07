98ec8c0fe20fc8599781a4f3b0102479999c858f
Sophia Shanthan <100741746+SophShan@users.noreply.github.com>
2023-08-07 12:20:51 -0400

    Update script.js

--- terrarium/script.js
+++ terrarium/script.js
@@ -20,29 +20,39 @@ function dragElement(terrariumElement) {
 		pos3 = 0,
 		pos4 = 0;
 	terrariumElement.onpointerdown = pointerDrag;
-	
+
 	function pointerDrag(e) {
 		e.preventDefault();
 		console.log(e);
+		// get the initial mouse cursor position for pos3 and pos4
 		pos3 = e.clientX;
 		pos4 = e.clientY;
+		// when the mouse moves, start the drag
 		document.onpointermove = elementDrag;
+		// when the mouse is lifted, stop the drag
 		document.onpointerup = stopElementDrag;
 	}
 
 	function elementDrag(e) {
-	pos1 = pos3 - e.clientX;
-	pos2 = pos4 - e.clientY;
-	pos3 = e.clientX;
-	pos4 = e.clientY;
-	console.log(pos1, pos2, pos3, pos4);
-	terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
-	terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
+		// calculate the new cursor position
+		// pos1 = where the Xmouse WAS - where it IS
+		pos1 = pos3 - e.clientX;
+		// pos2 = where the Ymouse WAS - where it IS
+		pos2 = pos4 - e.clientY;
+		//reset pos3 to current location of Xmouse
+		pos3 = e.clientX;
+		//reset pos4 to current location of Ymouse
+		pos4 = e.clientY;
+		console.log(pos1, pos2, pos3, pos4);
+		// set the element's new position:
+		terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
+		terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
 	}
 
 	function stopElementDrag() {
-	document.onpointerup = null;
-	document.onpointermove = null;
+		// stop calculating when mouse is released
+		document.onpointerup = null;
+		document.onpointermove = null;
 	}
 }
 
