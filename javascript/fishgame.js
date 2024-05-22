b8a71db3858581a48debcde6ca0c7213abf3d5b3
Sophia Shanthan <100741746+SophShan@users.noreply.github.com>
2024-05-22 14:33:12 -0400

    Update fishgame.js

--- javascript/fishgame.js
+++ javascript/fishgame.js
@@ -53,7 +53,7 @@ function spawnRandomObject() {
         xspeed: Math.random()*3.5 + 0.5,
         yspeed: Math.random()*3.5 +0.5,
 
-        diameter: Math.ceil (Math.random()*29+0.5),
+        diameter: Math.ceil (Math.random()*29+1),
     }
     
     // set spawn coordinates so they spawn at the edges using previous x to randomly choose where along edges they spawn
